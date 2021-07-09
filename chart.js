fa36be9b663a3c2c5b1d23a117407d64f78b00e1
Jamesrx33 <68870179+Jamesrx33@users.noreply.github.com>
2021-07-09 12:30:47 -0500

    Add Filters to Sample

--- chart.js
+++ chart.js
@@ -58,11 +58,12 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
+    let samples = data.samples
 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    let filtered = samples.filter(x => x.id == sample)
     //  5. Create a variable that holds the first sample in the array.
-
+    console.log(filtered)
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
 
@@ -71,16 +72,16 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    // var yticks = 
 
-    // 8. Create the trace for the bar chart. 
-    var barData = [
+    // // 8. Create the trace for the bar chart. 
+    // var barData = [
       
-    ];
-    // 9. Create the layout for the bar chart. 
-    var barLayout = {
+    // ];
+    // // 9. Create the layout for the bar chart. 
+    // var barLayout = {
      
-    };
+    // };
     // 10. Use Plotly to plot the data with the layout. 
     
   });
